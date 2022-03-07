52ca6bedb7da21d148436c5d2e8ee4239d16f67f
Tobias Petrasch <tobiaspetrasch@googlemail.com>
2022-03-07 19:01:34 +0100

    add dynamic port binding

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
