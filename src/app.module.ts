3bf33d41c8c9e9e0e73d78d9ee87299c2bc47933
Tobias Petrasch <tobiaspetrasch@googlemail.com>
2022-03-07 18:57:41 +0100

    add ssl reject unauthorized

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,17 +8,24 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: +configService.get<number>('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USERNAME'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DATABASE'),
-        ssl: configService.get<string>('POSTGRES_SSL') == 'true',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) => {
+        const config: any = {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: +configService.get<number>('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USERNAME'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DATABASE'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+
+        if (configService.get('POSTGRES_SSL') == 'true') {
+          config['ssl'] = { rejectUnauthorized: false };
+        }
+
+        return config;
+      },
       inject: [ConfigService],
     }),
     BackpackModule,
