6b2f9d17630d065bcd99aef74cb9da041e22184b
Tobias Petrasch <tobiaspetrasch@googlemail.com>
2022-03-07 21:50:33 +0100

    add empty validation to dtos

--- src/backpack/dto/createBackpack.dto.ts
+++ src/backpack/dto/createBackpack.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
-import { IsArray, IsString, ValidateNested } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 import { CreateBackpackItemDto } from './createBackpackItem.dto';
 
 export class CreateBackpackDto {
   @ApiProperty()
   @IsString()
   @Transform(({ value }) => value.toLowerCase())
+  @IsNotEmpty()
   readonly id: string;
 
   @ApiProperty({ type: () => CreateBackpackItemDto })
--- src/backpack/dto/createBackpackItem.dto.ts
+++ src/backpack/dto/createBackpackItem.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateBackpackItemDto {
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   readonly category: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   readonly source: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   readonly content: string;
 }
